5576e6d44eb0b8f832852a28b453143dc8ccffb4
Kelsey Merrill <kelseymerrill18@gmail.com>
2025-12-10 14:50:59 -0500

    add zokrates dir and script for running it

---
+++ generate-r1cs.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+mkdir -p zokrates/bin
+
+cd external/circ
+./driver.py -F zok zokc r1cs spartan smt
+
+./driver.py -b
+
+cd ../..
+./external/circ/target/release/examples/circ zokrates/main.zok --language zsharp-curly r1cs \
+    --action spartan-setup \
+    --prover-key zokrates/bin/P \
+    --verifier-key zokrates/bin/V
+
+./external/circ/target/release/examples/r1cs_inspect zokrates/bin/P > zokrates/bin/r1cs
+
+
