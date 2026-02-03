a5c77e902ba503118f0993f446cd05369509ba8d
Kelsey Merrill <kelseymerrill18@gmail.com>
2026-02-03 12:33:04 -0500

    randomized PIT test for dummy and non-dummy consistency

--- generate-r1cs.sh
+++ generate-r1cs.sh
@@ -14,14 +14,18 @@ mkdir -p zokrates/bin
 export RSMT2_CVC4_CMD=cvc5
 
 cd external/circ
-./driver.py -F zok zokc r1cs spartan smt
+./driver.py -F zok zokc r1cs spartan smt bellman
 
 ./driver.py -b
 
-./target/release/examples/circ "../../zokrates/$INPUT_FILE" --language zsharp-curly \
-    --field-custom-modulus 7237005577332262213973186563042994240857116359379907606001950938285454250989 r1cs \
-    --action spartan-setup \
+./target/release/examples/circ "../../zokrates/$INPUT_FILE" --language zsharp-curly r1cs \
+    --action setup \
+    --proof-impl dorian \
     --prover-key "../../zokrates/bin/${BASENAME}_P" \
     --verifier-key "../../zokrates/bin/${BASENAME}_V" \
 
-./target/release/examples/r1cs_inspect "../../zokrates/bin/${BASENAME}_P" > "../../zokrates/bin/${BASENAME}_r1cs"
+# ./target/release/examples/r1cs_inspect "../../zokrates/bin/${BASENAME}_P" > "../../zokrates/bin/${BASENAME}_r1cs"
+
+# This maybe goes on setup if we ever go back to spartan:
+ #    --field-custom-modulus 7237005577332262213973186563042994240857116359379907606001950938285454250989 r1cs \
+
